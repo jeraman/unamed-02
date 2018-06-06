efc17d834a73296081f2121ae8e3686ea1b2e1bd
jeraman <magodasmontanhascaminhantesdosul@hotmail.com>
2018-06-06 09:16:24 -0400

    adding note value to the metro for complex time signatures

--- src/soundengine/util/Metro.java
+++ src/soundengine/util/Metro.java
@@ -1,42 +1,91 @@
 package soundengine.util;
 
+/**
+ * Class that counts time in musical terms using BPM, bars, beats, and note
+ * values
+ * 
+ * @author jeronimo
+ *
+ */
 public class Metro implements Runnable {
-	
+
 	private int bpm;
-	private int bar;
-	private int beat;
-	
+	private int currentBar;
+	private int currentBeat;
+	private int currentNoteCount;
+	private int globalBeat;
+	private int globalNoteValue;
+
 	private Thread counter;
 	private boolean alive;
-	
+
+	public Metro() {
+		this(120);
+	}
+
 	public Metro(int bpm) {
+		this(bpm, 4, 4);
+	}
+
+	public Metro(int bpm, int globalBeat, int globalNoteValue) {
 		this.bpm = bpm;
-		this.bar = 0;
-		this.beat = 0;
+		this.globalBeat = globalBeat;
+		this.globalNoteValue = globalNoteValue;
+		this.currentBar = 0;
+		this.currentBeat = 0;
 		this.alive = false;
 	}
-	
-	public int getBar() {
-		return this.bar;
+
+	public int getBpm() {
+		return bpm;
+	}
+
+	public void setBpm(int bpm) {
+		this.bpm = bpm;
+	}
+
+	public int getCurrentBar() {
+		return currentBar;
+	}
+
+	public void setCurrentBar(int currentBar) {
+		this.currentBar = currentBar;
+	}
+
+	public int getCurrentBeat() {
+		return currentBeat + 1;
+	}
+
+	public void setCurrentBeat(int currentBeat) {
+		this.currentBeat = currentBeat;
+	}
+
+	public int getGlobalBeat() {
+		return globalBeat;
+	}
+
+	public void setGlobalBeat(int globalBeat) {
+		this.globalBeat = globalBeat;
+	}
+
+	public int getCurrentNoteCount() {
+		return currentNoteCount + 1;
+	}
+
+	public void setCurrentNoteCount(int currentNoteCount) {
+		this.currentNoteCount = currentNoteCount;
 	}
 	
-	public int getBeat() {
-		return this.beat;
+	public int getGlobalNoteValue() {
+		return globalNoteValue;
 	}
 	
-	public void setBar(int newBar) {
-		this.beat = newBar;
+	public void setGlobalNoteValue(int globalNoteValue) {
+		this.globalNoteValue = globalNoteValue;
 	}
-	
-	public void setBeat(int newBeat) {
-		this.beat = newBeat;
-	}
-	
+
 	public void stop() {
 		alive = false;
-		this.bar = 0;
-		this.beat = 0;
-		
 		try {
 			counter.join(0);
 		} catch (InterruptedException e) {
@@ -44,8 +93,12 @@ public class Metro implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void start() {
+		this.currentBar = 0;
+		this.currentBeat = 0;
+		this.currentNoteCount = 0;
+
 		alive = true;
 		Runnable r = this;
 		counter = new Thread(r);
@@ -54,15 +107,25 @@ public class Metro implements Runnable {
 
 	@Override
 	public void run() {
-		
-		while (alive) {
-			int time = (60 * 1000) / this.bpm;
-			Util.delay(time);
-			beat = (beat + 1) % 5;
 
-			if (beat == 0)
-				bar = bar + 1;
-		}	
+		while (alive) {
+			//int time = (int) ((60 * 1000) / (this.bpm * (this.globalNoteValue)));
+			int time = (int) 60 * 1000 / this.bpm;
+			
+			float mult = this.globalNoteValue/4f;
+			int adaptedTime = (int) (time/mult);
+			
+			Util.delay(adaptedTime);
+
+			//currentNoteCount = (currentNoteCount + 1) % this.globalNoteValue;
+			
+			//if (currentNoteCount == 0) {
+				currentBeat = (currentBeat + 1) % globalBeat;
+
+				if (currentBeat == 0)
+					currentBar = currentBar + 1;
+			//}
+		}
 	}
 
 }
--- tests/soundengine/util/MetroTest.java
+++ tests/soundengine/util/MetroTest.java
@@ -8,27 +8,36 @@ public class MetroTest extends PApplet {
 		PApplet.main("soundengine.util.MetroTest");
 	}
 
-	Metro m = new Metro(120);
+	Metro m = new Metro(120, 3, 8);
+
 	public void settings() {
 		size(800, 600);
 	}
 
 	public void setup() {
 		background(0);
-		m.start();
-	}
-	
-	public void mousePressed() {
-		println("stopping...");
-		m.stop();
-		println("restarting...");
-		m.start();
-	}
-	
-	public void draw() {
-		println("metro: " + m.getBar() + " : " + m.getBeat());
+		
+		if (counting)
+			m.start();
 	}
 
+	boolean counting = false;
+
+	public void mousePressed() {
+		if (counting) {
+			println("stopping...");
+			m.stop();
+			counting = false;
+		} else {
+			println("restarting...");
+			m.start();
+			counting = true;
+		}
+	}
+
+	public void draw() {
+		if (counting)
+			println("metro: " + m.getCurrentBar() + " : " + m.getCurrentBeat() + " : " + m.getCurrentNoteCount());
+	}
 
 }
-
